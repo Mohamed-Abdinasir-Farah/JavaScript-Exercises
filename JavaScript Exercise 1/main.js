8dbcd5567422514c826159a295401a161ca5133a
Mohamed-Abdinasir-Farah <mohamedabdinasir216@gmail.com>
2026-01-13 07:23:14 +0300

    JavaScript Exercise 1

---
+++ JavaScript Exercise 1/main.js
@@ -0,0 +1,19 @@
+let a = 7;
+let b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a / b);
+console.log(a * b);
+console.log(a % b);
+//Pre increment
+console.log(++a);
+//Post increment
+console.log(a++);
+console.log(a);
+
+//Pre decrement
+console.log(--b);
+//Post decrement
+console.log(b--);
+console.log(b);
