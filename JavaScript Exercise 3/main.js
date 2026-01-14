7e30f185e1ae07f2e7d5844535318d3280f76688
Mohamed-Abdinasir-Farah <mohamedabdinasir216@gmail.com>
2026-01-14 06:48:40 +0300

    JavaScript Exercise 3

---
+++ JavaScript Exercise 3/main.js
@@ -0,0 +1,9 @@
+let isSunny = true;
+let isWeekend = true;
+
+console.log(isSunny && isWeekend);    // Outputs true
+console.log(isSunny || isWeekend);    // Outputs true  
+console.log(!isSunny);                // Outputs false
+console.log(!isWeekend);             // Outputs false
+console.log(!isSunny && !isWeekend); // Outputs false
+console.log(!isSunny || isWeekend);  // Outputs  true
