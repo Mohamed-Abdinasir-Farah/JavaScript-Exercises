09ac6825c1018aa59487a17fd1d00496eaf27f97
Mohamed-Abdinasir-Farah <mohamedabdinasir216@gmail.com>
2026-02-05 21:48:57 +0300

    JavaScript Exercise 4

---
+++ JavaScript Exercise 4/main.js
@@ -0,0 +1,15 @@
+// Function Declaration
+function add(a,b) {
+  console.log(a + b);
+}
+add(10, 20);
+add(33,127);
+add(-6,156);
+
+// Function Expression
+const total = function(x,y) {
+  console.log(x + y);
+}
+total(100,  489);
+total(49, 127);
+total(18, 97);
