80e447d29fe8c3b64d5700663f083f3016efec41
Mohamed-Abdinasir-Farah <mohamedabdinasir216@gmail.com>
2026-02-08 07:22:48 +0300

    JavaScript Exercise 7

---
+++ JavaScript Exercise 7/main.js
@@ -0,0 +1,14 @@
+let colors = ["black", "white", "green"];
+console.log('before push');
+console.log(colors);
+
+console.log('after push');
+colors.push('red');
+console.log(colors);
+
+console.log('before shift');
+console.log(colors);
+
+console.log('after shift');
+colors.shift();
+console.log(colors);
