58c68bf83dab71d9d5ccc6a3aa5909c08e7eeedf
Mohamed-Abdinasir-Farah <mohamedabdinasir216@gmail.com>
2026-02-14 06:34:57 +0300

    JavaScript Exercise 10

---
+++ JavaScript Exercise 10/main.js
@@ -0,0 +1,16 @@
+let library = [
+  {
+    title : "The Guns of August",
+    author : "Barbara W. Tuchman",
+    year : 1962,
+  },
+
+    {
+    title : "The Rise and Fall of the Third Reich",
+    author : "William L. Shirer",
+    year : 1960,
+  }
+];
+
+console.log(library[0].title);
+console.log(library[1].author);
