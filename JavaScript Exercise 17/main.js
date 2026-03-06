c93999439bbf51953be32094c92a6e4f07516992
Mohamed-Abdinasir-Farah <mohamedabdinasir216@gmail.com>
2026-03-06 14:53:57 +0300

    JavaScript Exercise 17

---
+++ JavaScript Exercise 17/main.js
@@ -0,0 +1,10 @@
+let temperature = 10;
+if (temperature < 0) {
+  console.log("very cold")
+} else if (temperature <= 15) {
+  console.log("cold")
+} else if (temperature <= 25) {
+  console.log("warm")
+} else if (temperature > 25) {
+  console.log("hot")
+}
