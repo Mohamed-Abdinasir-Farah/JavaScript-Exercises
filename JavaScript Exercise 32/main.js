df7ee0f937e5bf254f0857c8b00542a0acdedb4a
Mohamed-Abdinasir-Farah <mohamedabdinasir216@gmail.com>
2026-04-03 17:43:54 +0300

    JavaScript Exercise 32

---
+++ JavaScript Exercise 32/main.js
@@ -0,0 +1,11 @@
+
+const header = document.querySelector('#header');
+
+console.log(header);
+
+const description = document.querySelectorAll('.description');
+
+console.log(description);
+
+const button = document.querySelectorAll('button')[0];
+console.log(button);
