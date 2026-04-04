21a84f317a46fed0f4575a355d699355654a2820
Mohamed-Abdinasir-Farah <mohamedabdinasir216@gmail.com>
2026-04-04 15:00:48 +0300

    JavaScript Exercise 33

---
+++ JavaScript Exercise 33/main.js
@@ -0,0 +1,8 @@
+
+function changeHeader() {
+  header.textContent = `Hello! good evening how are you doing?`
+}
+
+function changeParagraph() {
+  description.innerHTML = `This paragraph contains a message and it says i hope you you achieve great things in life`
+}
